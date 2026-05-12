3a0df0bd0eccec5abbb0a9c9a403907962f067b5
Zain Fong <1469433550@qq.com>
2026-05-12 18:36:13 +0800

    INIT

---
+++ app/src/main/java/com/example/myapplication/Word.java
@@ -0,0 +1,11 @@
+package com.example.myapplication;
+
+public class Word {
+    public String english;
+    public String chinese;
+
+    public Word(String english, String chinese) {
+        this.english = english;
+        this.chinese = chinese;
+    }
+}
